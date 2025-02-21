2323a7723720819147762b0d68ab1b6375c10207
Rishivandhan Musuvathi <rishimusuvathi03@gmail.com>
2025-02-21 15:41:15 -0500

    pcd to json

--- Assets/Scripts/EndScan.cs
+++ Assets/Scripts/EndScan.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.Networking;
 using Cdm.XR.Extensions;
+using Newtonsoft.Json;
+
 
 public class EndScan : MonoBehaviour
 {
@@ -15,34 +17,49 @@ public class EndScan : MonoBehaviour
 
     private NativeArray<Vector3> points;
 
-    void Start()
-    {
-        // densePointCloudManager.OnPointCloudUpdated += getPointsByUpdate;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
     public void onClick()
     {
+
+        if (densePointCloudManager == null)
+        {
+            Debug.Log("Densepointcloud manager is not assinged");
+            return;
+        }
+
         ARDensePointCloud pointCloud = densePointCloudManager.pointCloud;
         NativeArray<Vector3> points = pointCloud.points;
+
+        if (points == null)
+        {
+            Debug.Log("there are no points, hit the start button");
+            return;
+        }
+
+       
 
         for (int i = 0; i < Mathf.Min(points.Length, 5); i++)
         {
             Debug.Log($"Point {i}: {points[i]}");
         }
+
+
+
+        List<float[]> pointList = new List<float[]>();
+        foreach(Vector3 point in points)
+        {
+            pointList.Add(new float[] { point.x, point.y, point.z });
+
+        }
+    
+
+        string jsonData = JsonConvert.SerializeObject(pointList, Formatting.Indented);
+
+
            
     }
 
-    // UNUSED
-    public void getPointsByUpdate(ARDensePointCloud densePointCloud)
-    {
-        points = densePointCloud.points;
-    }
+    
+
+    
 
 }
