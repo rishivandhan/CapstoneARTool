870c667fb2241e6241bd3baf750245fa069c8dbe
Nate Kite <nathankite@gmail.com>
2025-02-21 17:37:04 -0500

    added cpd

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 import numpy as np
 import open3d as o3d
 import copy
+from pycpd import RigidRegistration
 
 app = Flask(__name__)
-MODEL_PATH = "./servertools/models/BoydPillarSimple.pcd"
+MODEL_PATH = "./servertools/models/Lab.pcd"
 
 reference_pcd = o3d.io.read_point_cloud(MODEL_PATH)
 	
@@ -23,8 +24,8 @@ def testdata():
 	
 	return "Data received successfully."
 
-@app.route('/localize', methods=['POST'])
-def localize():
+@app.route('/icp', methods=['POST'])
+def localize_icp():
 	try:
 		# Receive point cloud data as JSON
 		data = request.json
@@ -51,13 +52,13 @@ def localize():
 
 		# Visualize
 		# Color the clouds for clarity:
-		reference_pcd.paint_uniform_color([0, 0, 0])						# Reference in red
-		aligned_input.paint_uniform_color([0, 0, 1])				# Transformed input in green
-		input_pcd.paint_uniform_color([0, 0, 1])		# Original input in blue
+		reference_pcd.paint_uniform_color([1, 0, 0])		# Reference in red
+		aligned_input.paint_uniform_color([0, 1, 0])		# Transformed input in green
+		input_pcd.paint_uniform_color([0, 0, 1])			# Original input in blue
 
 		# Visualize both point clouds together
 		o3d.visualization.draw_geometries(
-			[reference_pcd, aligned_input],
+			[reference_pcd, aligned_input, input_pcd],
 			window_name="Alignment Visualization",
 			width=800,
 			height=600
@@ -65,6 +66,43 @@ def localize():
 
 		# Return transformation data to Unity
 		return jsonify({"transformation": transformation})
+	except Exception as e:
+		return jsonify({"error": str(e)})
+	
+@app.route('/cpd', methods=['POST'])
+def localize_cpd():
+	try:
+		# Receive point cloud data as JSON
+		data = request.json
+		input_points = np.array(data["points"])  # Expecting list of [x, y, z] points
+
+		print("registration")
+
+		# Apply CPD rigid registration
+		cpd = RigidRegistration(X=input_points, Y=np.asarray(reference_pcd.points))
+		transformed_source, transformation_matrix = cpd.register()
+
+		print("visualizing")
+
+		# Input to PCD (for visualization)
+		input_pcd = o3d.geometry.PointCloud()
+		input_pcd.points = o3d.utility.Vector3dVector(input_points)
+
+		# Visualize
+		reference_pcd.paint_uniform_color([1, 0, 0])		# Reference in red
+		transformed_source.paint_uniform_color([0, 1, 0])		# Transformed input in green
+		input_pcd.paint_uniform_color([0, 0, 1])			# Original input in blue
+
+		# Visualize both point clouds together
+		o3d.visualization.draw_geometries(
+			[reference_pcd, transformed_source, input_pcd],
+			window_name="Alignment Visualization",
+			width=800,
+			height=600
+		)
+
+		# Return transformation data to Unity
+		return jsonify({"transformation": "NOT IMPLEMENTED"})
 	except Exception as e:
 		return jsonify({"error": str(e)})
 	
