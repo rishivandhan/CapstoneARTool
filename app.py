78ea4721e5eed2f1377709ded77b510b35ddf752
Rishivandhan Musuvathi <rishimusuvathi03@gmail.com>
2025-03-25 17:26:13 -0400

    Added Transformation Test endpoint

--- app.py
+++ app.py
@@ -100,5 +100,14 @@ def visualize():
 	return 'Data received successfully.'
 
 
+@app.route('/transformation_test', methods=['POST'])
+def transformation_test():
+    transformation = [[1.0, 0.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, 0.0],
+              [0.0, 0.0, 1.0, 0.0],
+              [0.0, 0.0, 0.0, 1.0]]
+
+    return jsonify({"transformation": transformation})
+
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=5000)
